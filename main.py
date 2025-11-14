0cdeb4096e583d4ee71dcfb1f419a23a843f0c74
ANSBAditya <ansbaditya123@gmail.com>
2025-11-14 17:41:48 +0530

    The Bookbot prints character count in a neat format.(still hardcoded file path)

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-from stats import num_words
+from stats import *
 
 
 def main():
     book_content = get_book_text("books/frankenstein.txt")
+    sorted_list = get_sorted_list(get_char_count_dict(book_content))
+    
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt..")
+    print("----------- Word Count ----------")
     print(num_words(book_content))
+    print("--------- Character Count -------")
+    
+    #to print output of each alpha occurance in the format:
+    # 'char: count'
+    for element in sorted_list:
+        if element["char"].isalpha():
+            print(f"{element['char']}: {element['num']}", end='\n')
+    
+    print("============= END ===============")
+
 
 
 def get_book_text(file_path):
--- stats.py
+++ stats.py
@@ -3,3 +3,26 @@ def num_words(content):
     word_list = content.split()
     return f"Found {len(word_list)} total words"
 
+def get_char_count_dict(content):
+    char_log_dict = {}
+
+    for char in content.lower():
+        try:
+            char_log_dict[char] += 1
+        except KeyError:
+            char_log_dict[char] = 1
+    
+    return char_log_dict
+
+def get_sorted_list(char_dict):
+    final_list = []
+
+    for key in char_dict:
+        list_element = {"char": key, "num": char_dict[key]}
+        final_list.append(list_element)
+    
+    final_list.sort(reverse=True, key=sort_on)
+    return final_list
+
+def sort_on(element):
+    return element["num"]
